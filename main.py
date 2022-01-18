b35352d44e7aa14985a60f29f0a407c3745b298e
User <vsevoloduzaninov@gmail.com>
2022-01-18 16:51:10 +0500

    hello

--- main.py
+++ main.py
@@ -6,5 +6,5 @@
     git commit -m "first commit"
     git branch -M main
     git remote add origin https://github.com/pypython10/djungo.git
-    git push -u origin main
+   ggg git push -u origin main
 '''
