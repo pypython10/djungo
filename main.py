3b43d21c98e5608b4552c7cedc85b435d0c6c40e
User <vsevoloduzaninov@gmail.com>
2022-01-18 16:44:25 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Как загрузить файлы в гит
+1.скачать гит на пк
+2.  git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/pypython10/djungo.git
+    git push -u origin main
+'''
